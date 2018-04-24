a724b3305419f55647311b2b993dd793f55db732
unknown <mail4khusanboy@gmail.com>
2018-04-23 20:11:46 -0400

    Eat da burger done

---
+++ config/connection.js
@@ -0,0 +1,18 @@
+
+var mysql = require('mysql');
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    port     :  3307,
+    user     : 'root',
+    password : '', 
+    database : 'burgers_db' 
+  });
+}
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO burgers(burger_name, devoured, date) VALUES("Quarter Pounder", false, "2016-11-05 16:20:01");
+INSERT INTO burgers(burger_name, devoured, date) VALUES("Whooper", false, "2016-11-05 04:20:01");
+INSERT INTO burgers(burger_name, devoured, date) VALUES("Cheezburger", false, "2016-11-05 04:20:02");
+INSERT INTO burgers(burger_name, devoured, date) VALUES("Baconator", false, "2016-11-05 16:20:00");
+INSERT INTO burgers(burger_name, devoured, date) VALUES ("Big Mac", false, "2016-11-05 04:20:00");
+INSERT INTO burgers(burger_name, devoured, date) VALUES("McRoyal", false, "2016-11-05 04:20:20");
+INSERT INTO burgers(burger_name, devoured, date) VALUES("Double Double", true, "2016-11-05 16:20:16");
